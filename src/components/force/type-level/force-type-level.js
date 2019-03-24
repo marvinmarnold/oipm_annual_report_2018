08985bf65821a8abac17bde0c2c9637e336e402d
Marvin Arnold <marvin@geeky.rocks>
2019-03-24 15:38:23 -0500

    Added bullets to level graphs

--- src/components/force/type-level/force-type-level.js
+++ src/components/force/type-level/force-type-level.js
@@ -16,7 +16,36 @@ class ForceTypeLevel extends React.Component {
 		return (
 			<div className="my-3">
 				<Row>
-					<Col><h4>Amount of force (UOF) by level</h4></Col>
+					<Col>
+						<h4>Amount of force (UOF) by level</h4>
+						<strong>Level 1</strong>
+						<ul>
+							<li>Level 1 force decreased for the third year in a row.</li>
+							<li>From 2017-2018, Level 1 force decreased by 266 UOF or 177% more than from 2016-2017. </li>
+							<li>Both taser and firearm exhibition has decreased significantly since 2017. </li>
+						</ul>
+
+						<strong>Level 2</strong>
+						<ul>
+							<li>Level 2 UOF decreased substantially in 2018, dropping from 382 in 2017 to 182 in 2018. This is the lowest Level 2 has been since 2015. </li>
+							<li>Defense tech/take down decreased by approximately 360%.</li>
+							<li>Taser use has averaged about 57 UOF from 2016 to 2018 and remained consistent.</li>
+							<li>Canine (no bite) has experienced little changed from 2017 to 2018, hovering near 20 UOF. </li>
+						</ul>
+
+						<strong>Level 3</strong>
+						<ul>
+							<li>Level 3 force has doubled for the second year in a row.  </li>
+							<li>Since 2015, head strike (no weapon) has been steadily increasing up to 10 in 2018. </li>
+						</ul>
+
+						<strong>Level 4</strong>
+						<ul>
+							<li>Level 4 force has decreased yearly since 2015. </li>
+							<li>There was no firearm discharged UOF in 2018. </li>
+							<li>While level 4 UOF has decreased overall, head strikes while handcuffed increased from 1 in 2017 to 6 in 2018. </li>
+						</ul>
+					</Col>
 				</Row>
 				<Row>
 					<Col>

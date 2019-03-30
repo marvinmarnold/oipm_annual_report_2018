ed283b5150cbe61254d500a1e6c2c887c0b01c07
Marvin Arnold <marvin@geeky.rocks>
2019-03-30 17:46:43 -0500

    Force CDF by top offending officer bucket

--- src/components/force/most-forceful/most-forceful-section.js
+++ src/components/force/most-forceful/most-forceful-section.js
@@ -3,6 +3,7 @@ import { Row, Col } from 'reactstrap'
 import { Link } from "gatsby"
 
 import ForcePerOfficerPlot from './force-per-officer-plot.js'
+import ForceCdfOfficersPlot from './force-cdf-officers-plot.js'
 
 class MostForcefulSection extends React.Component {
 	constructor() {
@@ -28,6 +29,27 @@ class MostForcefulSection extends React.Component {
 		)
 	}
 
+	renderForceCdfOfficers() {
+		return (
+			<div>
+				<Row>
+					<Col>
+						<h4>Average force per officer</h4>
+						<ul>
+							<li>How to read the graph: The vertical axis is percentage and horizontal axis is number of officers. The graph is cumulative, meaning that the top 10 officers includes the top 5 officers. </li>
+							<li>The top 10 officers are responsible for 11.31 of all FTN in 2018, up slightly from 10.38% in 2017.</li>
+							<li>The top 10 officers are responsible for 14.17% of UOF in 2018, a negligible increase from 13.34% in 2017.</li>
+							<li>The top 400 officers with FTNs are responsible for 100% of force in 2018. In 2017, 500 officers were responsible for total force.</li>
+						</ul>
+					</Col>
+					<Col>
+						<ForceCdfOfficersPlot />
+					</Col>
+				</Row>
+			</div>
+		)
+	}
+
 	render() {
 		return (
 			<div>
@@ -36,6 +58,7 @@ class MostForcefulSection extends React.Component {
 				</Row>
 
 				{this.renderForcePerOfficer()}
+				{this.renderForceCdfOfficers()}
 
 			</div>
 		)

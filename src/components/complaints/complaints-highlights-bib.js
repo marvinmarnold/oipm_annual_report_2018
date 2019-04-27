7cdf45d909de62ebbcff4208153efb787b33a236
Marvin Arnold <marvin@geeky.rocks>
2019-04-27 14:46:40 -0500

    Most common allegations

--- src/components/complaints/complaints-highlights-bib.js
+++ src/components/complaints/complaints-highlights-bib.js
@@ -12,6 +12,10 @@ const ComplaintsHighlightsBib = () => (
 			<p>Government of the District of Colombia, Police Complaints Board, Office of Police Complaints, Annual Report 2018, pg. 6, from https://policecomplaints.dc.gov/page/annual-reports-for-OPC, https://policecomplaints.dc.gov/node/1366436, accessed April 9, 2019.</p>
 			<p>Toledo Police Department, The Toledo Police Annual Report: 1867-2017, 150 years of service, pg. 35, from https://toledopolice.com/, https://www.toledopolicemuseum.com/tpd-2018-annual-report, accessed April 9, 2019.</p>
 		</Col>
+
+		<h6 id="ref-2-common">[2]</h6>
+		<p>Chicago Police Department, Chicago Police Department: Annual Report 2017, p. 28-30, from https://home.chicagopolice.org/inside-the-cpd/statistical-reports/annual-reports/, https://home.chicagopolice.org/wp-content/uploads/2019/03/Chicago-Police-Department-Annual-Report-2017.pdf, accessed April 9, 2019.</p>
+		<p>Government of the District of Colombia, Police Complaints Board, Office of Police Complaints, Annual Report 2018, pg. 7, from https://policecomplaints.dc.gov/page/annual-reports-for-OPC, https://policecomplaints.dc.gov/node/1366436, accessed April 9, 2019</p>
 	</Row>
 )
 
--- src/pages/complaints-highlights.js
+++ src/pages/complaints-highlights.js
@@ -8,6 +8,7 @@ import ComplaintsHighlightsBib from '../components/complaints/complaints-highlig
 
 import ComplaintsByYearTypePlot from '../components/complaints/time/complaints-by-year-type-plot'
 import AllegationsByYearTypePlot from '../components/complaints/time/allegations-by-year-type-plot'
+import AllegationsMostCommonPlot from '../components/complaints/allegations-most-common-plot'
 
 class ComplaintsHighlightsPage extends React.Component {
   constructor() {
@@ -23,6 +24,21 @@ class ComplaintsHighlightsPage extends React.Component {
 					<NavLink href="#complaints-by-year" className="text-white">Complaints and allegations by year</NavLink>
 				</NavItem>
 				<NavItem>
+					<NavLink href="#alleg-most-common" className="text-white">Most common allegations</NavLink>
+				</NavItem>
+				<NavItem>
+					<NavLink href="#complaints-outcomes" className="text-white">Complaint outcomes</NavLink>
+				</NavItem>
+				<NavItem>
+					<NavLink href="#complaints-4th" className="text-white">4th amendment complaints</NavLink>
+				</NavItem>
+				<NavItem>
+					<NavLink href="#complaints-anonymous" className="text-white">Anonymous complaints</NavLink>
+				</NavItem>
+				<NavItem>
+					<NavLink href="#complaints-discipline" className="text-white">Discipline</NavLink>
+				</NavItem>
+				<NavItem>
 					<NavLink href="#complaints-bib" className="text-white">Bibliography</NavLink>
 				</NavItem>
 			</Nav>
@@ -35,7 +51,7 @@ class ComplaintsHighlightsPage extends React.Component {
 				<Row>
 					<Col>
 						<h4>Complaints &amp; Allegations by Year </h4>
-						<p>Complaints and allegations both appeared to have a slight decrease in 2018 compared with 2017. This appears to be on trend with other United States police departments of similar size, which are experiencing stagnation in complaints or decreases <a href="#ref-1-boulder">[1]</a>. Given that complaint decreases are taking place in metropolitan police departments across the U.S., this may indicate a universal reform in policing over the past few years.  Some have posited that the decreased is due to political and social pressure.</p>
+						<p>Complaints and allegations both appeared to have a slight decrease in 2018 compared with 2017. This appears to be on trend with other United States police departments of similar size, which are experiencing stagnation in complaints or decreases <a href="#ref-1-boulder">[1]</a>. Given that complaint decreases are taking place in metropolitan police departments across the U.S., this may indicate a universal reform in policing over the past few years. Some have posited that the decreased is due to political and social pressure.</p>
 					</Col>
 				</Row>
 				<Row>
@@ -50,6 +66,25 @@ class ComplaintsHighlightsPage extends React.Component {
 				</Row>
 			</div>
 
+		)
+	}
+
+	renderMostCommon() {
+		return (
+			<div id="alleg-most-common">
+				<Row>
+					<Col>
+						<h4>Most common allegations</h4>
+						<p>According to NOPD data, the three most common complaint allegations in 2018, representing roughly 80% of all allegations, were: “neglect of duty,” “professionalism,” and “adherence to law.” Similar to findings analyzed of other departments, complaints tend to originate from civilian interactions <a href="#ref-2-common">[2]</a>.</p>
+						<p>Complaint allegations have shifted slightly from 2017. While “neglect of duty” and “professionalism” remained the most common allegations, the third most common switched from “instructions from an authoritative source” in 2017 to “adherence to law” in 2018. While there was not a significant percentage change in “adherence to law” allegations between 2017 and 2018, “instructions from an authoritative source” allegations decreased by almost sixty (60) instances, or over 3%.</p>
+					</Col>
+				</Row>
+				<Row>
+					<Col>
+						<AllegationsMostCommonPlot />
+					</Col>
+				</Row>
+			</div>
 		)
 	}
 
@@ -68,6 +103,7 @@ class ComplaintsHighlightsPage extends React.Component {
 						</Row>
 
 						{this.renderOverTime()}
+						{this.renderMostCommon()}
 						<ComplaintsHighlightsBib />
 					</div>
 				}
